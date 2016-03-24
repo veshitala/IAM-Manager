3d30e73c0f089167b1a62b12fe30326a6ea7a6f7
swetha.naretla <swetha.naretla@micropyramid.com>
2016-03-24 11:39:10 +0530

    Deployed on heroku

--- iam_manager/settings.py
+++ iam_manager/settings.py
@@ -89,6 +89,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = (os.path.join(BASE_DIR, "static"))
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',

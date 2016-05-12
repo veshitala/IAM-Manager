d4908eb6618dd485df40bfdefb4522d0db28da4e
swetha.naretla <swetha.naretla@micropyramid.com>
2016-05-12 13:11:34 +0530

    Solved database issue while deploying to heroku

--- iam_manager/settings.py
+++ iam_manager/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -71,11 +73,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
 
 # Static files (CSS, JavaScript, Images)

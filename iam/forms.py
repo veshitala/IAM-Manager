0ce865d58a4e39d5e3ddd5fcb9ce2260cc60c374
swetha.naretla <swetha.naretla@micropyramid.com>
2016-05-27 13:40:07 +0530

    Updated change password view, when password not created while creating user

--- iam/forms.py
+++ iam/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class BucketForm(forms.Form):
     bucket_name = forms.CharField(max_length=100)
     region_name = forms.CharField(max_length=100)
+
+
+class ChangePasswordForm(forms.Form):
+    new_password = forms.CharField(max_length=150)
+    confirm_password = forms.CharField(max_length=150)
